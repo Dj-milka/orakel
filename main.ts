43cd3437263e6c5d742a65afd6a349360578f88f
Dj-milka <128476427+Dj-milka@users.noreply.github.com>
2023-03-21 12:06:43 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+let _1 = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.B) || (input.buttonIsPressed(Button.AB) || (input.buttonIsPressed(Button.A) || input.isGesture(Gesture.Shake)))) {
+        _1 = randint(1, 3)
+    }
+    if (_1 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    }
+    if (_1 == 2) {
+        basic.showLeds(`
+            . # # . .
+            . . . # .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+    }
+    if (_1 == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
 })
